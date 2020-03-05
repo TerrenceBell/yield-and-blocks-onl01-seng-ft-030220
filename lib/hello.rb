d7ae00aed99aa5e2a9eb41ace4039848bca26b3f
Terrence Bell <terrencekbell@gmail.com>
2020-03-05 06:01:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,7 @@
-def hello_t(array)
-i = 0
-while i < array.length
-i = i + 1
-end
- end
-# call your method here!
 
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
